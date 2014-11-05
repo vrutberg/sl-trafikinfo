8badb6aa3f4043a75d5362ccde91851fe4c5c7b8
Viktor Rutberg <vrutberg@atex.com>
2014-11-05 15:11:42 +0100

    Improved error handling around environment.json /vrutberg

--- slapi.py
+++ slapi.py
@@ -15,14 +15,23 @@ class SlApi:
         return
 
     def _get_api_key(self):
-        if self._configuration_object is None:
-            f = open("environment.json", "r")
-            lines = f.readlines()
-            f.close()
+        try:
+            if self._configuration_object is None:
+                f = open("environment.json", "r")
+                lines = f.readlines()
+                f.close()
+                self._configuration_object = json.loads(''.join(lines))
 
-            self._configuration_object = json.loads(''.join(lines))
-
-        return self._configuration_object[self._api_key_key]
+            return self._configuration_object[self._api_key_key]
+        except IOError:
+            print "Could not open 'environment.json' for reading"
+            raise
+        except ValueError:
+            print "Could not parse 'environment.json', perhaps it contains invalid JSON?"
+            raise
+        except KeyError as e:
+            print "Could not find key '" + e.message + "' in 'environment.json'"
+            raise
 
     def get_base_url(self):
         return self._endpoint_url + "?key=" + self._get_api_key()
